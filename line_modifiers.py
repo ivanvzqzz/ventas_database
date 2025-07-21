1d0216650ed9dba623504b19d587d2c597f3befc
ivanvzqzz <vazzp.ctd@gmail.com>
2025-07-20 20:21:30 -0700

    Update line_modifiers.py
    Add as function

--- line_modifiers.py
+++ line_modifiers.py
@@ -11,60 +11,64 @@ def generate_line_modifiers_id(modifier_id, item_id):
     name = f"{modifier_id}-{item_id}"
     return str(uuid.uuid5(namespace, name))
 
-conn = psycopg2.connect(
-    dbname = os.getenv("DB_NAME"),
-    user = os.getenv("DB_USER"),
-    password = os.getenv("DB_PASSWORD"),
-    host = os.getenv("DB_HOST"),
-    port = os.getenv("DB_PORT")
-    )
+def insert_line_modifiers():
+    conn = psycopg2.connect(
+        dbname = os.getenv("DB_NAME"),
+        user = os.getenv("DB_USER"),
+        password = os.getenv("DB_PASSWORD"),
+        host = os.getenv("DB_HOST"),
+        port = os.getenv("DB_PORT")
+        )
 
-cursor = conn.cursor()
+    cursor = conn.cursor()
 
-with open("receipts.json", "r", encoding="utf-8") as f:
-    receipts = json.load(f)
+    with open("receipts.json", "r", encoding="utf-8") as f:
+        receipts = json.load(f)
 
-insert_line_modifiers_query = """
-INSERT INTO line_modifiers (id, receipt_number, line_items_id, line_modifier_id, modifier_option_id,
-                            name, option, price, money_amount)
-VALUES(
-                            %s, %s, %s, %s, %s,
-                            %s, %s, %s, %s
-                            )
-ON CONFLICT (line_modifier_id, line_items_id) DO NOTHING
-"""
+    insert_line_modifiers_query = """
+    INSERT INTO line_modifiers (id, receipt_number, line_items_id, line_modifier_id, modifier_option_id,
+                                name, option, price, money_amount)
+    VALUES(
+                                %s, %s, %s, %s, %s,
+                                %s, %s, %s, %s
+                                )
+    ON CONFLICT (line_modifier_id, line_items_id) DO NOTHING
+    """
 
-inserted_count = 0
+    inserted_count = 0
 
-for receipt in receipts:
-    receipt_number = receipt.get("receipt_number")
-    
-    for line_item in receipt.get("line_items", []):
-        line_items_id = line_item.get("id")
+    for receipt in receipts:
+        receipt_number = receipt.get("receipt_number")
+        
+        for line_item in receipt.get("line_items", []):
+            line_items_id = line_item.get("id")
 
-        for item in line_item.get("line_modifiers", []):
-            cursor.execute(insert_line_modifiers_query, (
-                generate_line_modifiers_id(item.get("id"), line_items_id),
-                receipt_number,
-                line_items_id,
-                item.get("id"),
-                item.get("modifier_option_id"),
-                item.get("name"),
-                item.get("option"),
-                item.get("price"),
-                item.get("money_amount")
-            ))
+            for item in line_item.get("line_modifiers", []):
+                cursor.execute(insert_line_modifiers_query, (
+                    generate_line_modifiers_id(item.get("id"), line_items_id),
+                    receipt_number,
+                    line_items_id,
+                    item.get("id"),
+                    item.get("modifier_option_id"),
+                    item.get("name"),
+                    item.get("option"),
+                    item.get("price"),
+                    item.get("money_amount")
+                ))
 
-            if cursor.rowcount == 1:
-                inserted_count +=1
+                if cursor.rowcount == 1:
+                    inserted_count +=1
 
-conn.commit()
-if inserted_count == 0:
-    print("No new records available")
-elif inserted_count == 1:
-    print("Updated sales database, added 1 record to line_items table.")
-else:
-    print(f"Updated sales database, added {inserted_count} records to line_items table.")
+    conn.commit()
+    if inserted_count == 0:
+        print("No new records available")
+    elif inserted_count == 1:
+        print("Updated sales database, added 1 record to line_items table.")
+    else:
+        print(f"Updated sales database, added {inserted_count} records to line_items table.")
 
-cursor.close()
-conn.close()
+    cursor.close()
+    conn.close()
+
+if __name__ == "__main__":
+    insert_line_modifiers()
